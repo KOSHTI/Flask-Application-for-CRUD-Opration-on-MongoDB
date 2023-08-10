c06564c38f6f683ee1719973fb2b8c6b76e189d8
Shubham Koshti <98097225+KOSHTI@users.noreply.github.com>
2023-08-11 01:06:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,60 +2,55 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId
 import json
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client['your_database_name']
 collection = db['users']
 
-# to convert objectId into json format
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return super().default(o)
 
-# Get all users
-@app.route('/users', methods=['GET'])
-def get_all_users():
-    users = list(collection.find({}))
-    # to convert data in json format (thik hai......!!)
-    return JSONEncoder().encode(users)
-
-# Get single user by id
-@app.route('/users/<string:user_id>', methods=['GET'])           # userid is in a string format
-def get_user(user_id):
-    user = collection.find_one({'_id': ObjectId(user_id)}, {'_id': 0})
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({'message': 'User not found'}), 404
-
-# Create a new user
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    user_id = collection.insert_one(data).inserted_id
-    return jsonify({'message': 'User created successfully', 'user_id': str(user_id)})
-
-# Update user by id
-@app.route('/users/<string:user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.get_json()
-    result = collection.update_one({'_id': ObjectId(user_id)}, {'$set': data})
-    if result.modified_count == 1:
-        return jsonify({'message': 'User updated successfully'})
-    else:
-        return jsonify({'message': 'User not found'}), 404
-
-# Delete user by id
-@app.route('/users/<string:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    result = collection.delete_one({'_id': ObjectId(user_id)})
-    if result.deleted_count == 1:
-        return jsonify({'message': 'User deleted successfully'})
-    else:
-        return jsonify({'message': 'User not found'}), 404
+class User(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            user = collection.find_one({'_id': ObjectId(user_id)}, {'_id': 0})
+            if user:
+                return jsonify(user)
+            else:
+                return {'message': 'User not found'}, 404
+        else:
+            users = list(collection.find({}))
+            return JSONEncoder().encode(users)
     
+    def post(self):
+        data = request.get_json()
+        user_id = collection.insert_one(data).inserted_id
+        return {'message': 'User created successfully', 'user_id': str(user_id)}
+    
+    def put(self, user_id):
+        data = request.get_json()
+        result = collection.update_one({'_id': ObjectId(user_id)}, {'$set': data})
+        if result.modified_count == 1:
+            return {'message': 'User updated successfully'}
+        else:
+            return {'message': 'User not found'}, 404
+    
+    def delete(self, user_id):
+        result = collection.delete_one({'_id': ObjectId(user_id)})
+        if result.deleted_count == 1:
+            return {'message': 'User deleted successfully'}
+        else:
+            return {'message': 'User not found'}, 404
+
+# Add the User resource to the API with specific routes
+api.add_resource(User, '/users', '/users/', '/users/<string:user_id>')
+
 if __name__ == '__main__':
     app.run(debug=True)
